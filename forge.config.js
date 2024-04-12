ba559a24642b5cae965c5a1728a4f2494da60ad0
cryptox427 <smithzhang0427@gmail.com>
2024-04-13 01:58:46 +0800

    add forge.config.js

---
+++ forge.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    packagerConfig: {},
+    makers: [
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {
+                authors: 'My Name',
+                description: 'My Description',
+            },
+        }
+    ]
+};
